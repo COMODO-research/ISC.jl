54631551c7cfc5de3cb336947908c3f78377b56e
kamrancivileng <your_email@example.com>
2026-04-08 10:42:50 +0100

    added new function shift_elements.

--- examples/demo_shift_elements.jl
+++ examples/demo_shift_elements.jl
@@ -1,4 +1,5 @@
 using GeometryBasics
+using ISC
 # PART 1
 V1 = [Point(0.0, 0.0, 0.0),Point(1.0, 0.0, 0.0),Point(1.0, 1.0, 0.0),Point(0.0, 1.0, 0.0),
       Point(0.0, 0.0, 1.0),Point(1.0, 0.0, 1.0), Point(1.0, 1.0, 1.0)Point(0.0, 1.0, 1.0)]
